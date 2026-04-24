95f33be6a1c55f202f56ef36897a757d64d91a39
Kexin Wang <kexin.wang@estudiantat.upc.edu>
2026-04-24 16:31:09 +0200

    afegit loadarilines and loadairports

--- LEBL.py
+++ LEBL.py
@@ -1,3 +1,5 @@
+import os
+
 class Gate:
     "Represents an individual boarding gate"
     def __init__(self, name):
@@ -29,24 +31,100 @@ class BarcelonaAP:
 
 def SetGates(area, init_gate, end_gate, prefix):
 
-    # 1. Validation: end_gate must be greater than init_gate
     if end_gate <= init_gate:
         return -1
 
-    # 2. Drop previous list of gates (resetting the list)
+    # 2. Drop previous list of gates
     area.gates = []
 
     gate_num = init_gate
-
     while gate_num <= end_gate:
         gate_name = f"{prefix}{gate_num}"
-
         # We create the object Gate i we added to the area
         new_gate = Gate(gate_name)
         area.gates.append(new_gate)
-
-        # INCREMENT MANUAL: passem a la següent porta
         gate_num += 1
 
     return 0
 
+def LoadAirlines(terminal, t_name):
+    filename = f"{t_name}_Airlines.txt"
+
+    #We check if the file exists
+    if not os.path.exists(filename):
+        return -1 #File doesn't exist
+
+    #If the file exists...
+    terminal.airlines = []
+
+    try:
+        f = open(filename, "r")
+        lines = f.readlines()
+        f.close()
+
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+            if line:
+                parts = line.split('\t')
+                icao_code = parts[-1] #The ICAO code is the las element of the line
+                terminal.airlines.append(icao_code)
+            i += 1
+        return 0
+
+    except Exception:
+        return -1
+
+def LoadAirportStructure(filename):
+    if not os.path.exists(filename):
+        return None
+    try:
+        f = open(filename, "r")
+        lines = f.readlines()
+        f.close()
+
+        header = lines[0].split()
+        icao_code = header[0]
+        num_terminals = int(header[1])
+
+        airport = BarcelonaAP(icao_code)
+
+        current_line = 1
+        t_count = 0
+
+        while t_count < num_terminals:
+            t_data = lines[current_line].split()
+            t_name = t_data[1]
+            num_areas = int(t_data[2])
+
+            terminal_obj = Terminal(t_name)
+
+            LoadAirlines(terminal_obj, t_name)
+
+            current_line += 1
+            a_count = 0
+
+            while a_count < num_areas:
+                a_data = lines[current_line].split()
+                area_name = a_data[1]
+                area_type = a_data[2]
+                init_g = int(a_data[4])
+                end_g = int(a_data[6])
+
+                area_obj = BoardingArea(area_name, area_type)
+
+                gate_prefix = f"{t_name}{area_name}G"
+                SetGates(area_obj, init_g, end_g, gate_prefix)
+
+                terminal_obj.boarding_areas.append(area_obj)
+
+                current_line += 1
+                a_count += 1
+
+            airport.terminals.append(terminal_obj)
+            t_count += 1
+
+        return airport
+
+    except Exception:
+        return None
