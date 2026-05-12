70a9ed05594e1c0f44ed4a5511b94c68b996cf8a
Erika Rubí <erika.rubi@estudiantat.upc.edu>
2026-05-12 12:19:46 +0200

    -

--- aircraft.py
+++ aircraft.py
@@ -2,7 +2,6 @@ import math
 import matplotlib.pyplot as plt
 import os
 from airport import IsSchengenairport, LoadAirports, FindAirport
-from airport import IsSchengenairport, LoadAirports, FindAirport
 
 class Aircraft:
     def __init__(self, aircraft_id, airline, origin, time):
